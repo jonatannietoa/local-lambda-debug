8a177ad1bf647f82a46be4ecf3766ed9fd343e91
Jonatan Nieto <jonatan.nieto@sage.com>
2017-11-30 12:24:32 +0100

    Add RequestMapping

--- echo/index.js
+++ echo/index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const hello = (event, context, callback) => {
+  console.log('Launching Provisioning Services');
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Launching Provisioning Services in Serverless!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
 const echo = function (event, context, callback) {
 	console.log(event, context);
 	return callback(null, {
@@ -12,3 +28,4 @@ const echo = function (event, context, callback) {
 };
 
 module.exports.echo = echo;
+module.exports.hello = hello;
